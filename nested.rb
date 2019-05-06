269f0b51c6fd740c10192480adc0aafa77bd1cab
Gary Wheeler <gkwheeler40@gmail.com>
2019-05-06 15:16:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	alan_kay_is_known_for[:alan_kay][:known_for]
+	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
 end
 
 def dennis_ritchies_language
